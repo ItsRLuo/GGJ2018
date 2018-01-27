9a7475421752775f40406ebc5280260807758314
Samuel Gao <samuel.gao3@gmail.com>
2018-01-27 09:44:45 -0800

    Added Scene for switchboard.

---
+++ Assets/_Game/Scripts/Controllers/SwitchController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+
+	public float smooth = 2.0F;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+	void OnTriggerEnter(){
+		Debug.Log (tag);
+		if (tag == "UpDownSwitch") {
+//			Quaternion target = Quaternion.Euler(0f, 0f, -30f);
+//			transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+			var rotationVector = transform.rotation.eulerAngles;
+			rotationVector.z = -120;
+			transform.rotation = Quaternion.Euler (rotationVector);
+		} else if (tag == "LeftRightSwitch"){
+			var rotationVector = transform.rotation.eulerAngles;
+			rotationVector.y = 30;
+			transform.rotation = Quaternion.Euler (rotationVector);
+		}
+	}
+}
