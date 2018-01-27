19bbbf40e45db8ea892feb7db9b074e95f292cf9
Tedo Pranowo <tedokdr@yahoo.com>
2018-01-27 00:34:12 -0800

    Add collision to the hand prefabs

---
+++ Assets/_Game/Scripts/Temp/AlwaysInFrontCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysInFrontCamera : MonoBehaviour {
+    [SerializeField] private Camera m_mainCamera;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = m_mainCamera.transform.position + m_mainCamera.transform.forward * 1.0f;
+	}
+}
