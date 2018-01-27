630bd4c050e7329662a1bdc3d68a6b196b413bb4
Samuel Gao <samuel.gao3@gmail.com>
2018-01-27 10:08:08 -0800

    Added ability to turn switch on/off.

--- Assets/_Game/Scripts/Controllers/SwitchController.cs
+++ Assets/_Game/Scripts/Controllers/SwitchController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SwitchController : MonoBehaviour {
 
 	public float smooth = 2.0F;
+	private int isTurnOn = -1;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,17 +16,20 @@ public class SwitchController : MonoBehaviour {
 		
 	}
 
-	void OnTriggerEnter(){
+	void OnTriggerExit(){
 		Debug.Log (tag);
 		if (tag == "UpDownSwitch") {
 //			Quaternion target = Quaternion.Euler(0f, 0f, -30f);
 //			transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
 			var rotationVector = transform.rotation.eulerAngles;
-			rotationVector.z = -120;
+			rotationVector.z = 120 * isTurnOn;
+			isTurnOn *= -1;
 			transform.rotation = Quaternion.Euler (rotationVector);
+
 		} else if (tag == "LeftRightSwitch"){
 			var rotationVector = transform.rotation.eulerAngles;
-			rotationVector.y = 30;
+			rotationVector.y = 30 * isTurnOn;
+			isTurnOn *= -1;
 			transform.rotation = Quaternion.Euler (rotationVector);
 		}
 	}
