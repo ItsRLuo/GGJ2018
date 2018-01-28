9402685c7b4253bbb6a955f2ad3a9115e3cbdc8f
Tedo Pranowo <tedokdr@yahoo.com>
2018-01-28 11:24:57 -0800

    Add notification script

---
+++ Assets/_Game/Scripts/Managers/NotificationHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationHandler : MonoBehaviour {
+
+    //---------------------------------------------------------------------------------------------
+    // Singleton
+    //---------------------------------------------------------------------------------------------
+    private static NotificationHandler s_instance;
+    public static NotificationHandler instance
+    {
+        get { return s_instance; }
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // Variables
+    //---------------------------------------------------------------------------------------------
+    [SerializeField] private Text m_notificationText;
+
+    //---------------------------------------------------------------------------------------------
+    // MonoBehaviors
+    //---------------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        if (s_instance == null)
+            s_instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        s_instance = null;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // Functions
+    //---------------------------------------------------------------------------------------------
+    public void Notify(string message)
+    {
+        gameObject.SetActive(true);
+        m_notificationText.text = message;
+    }
+
+    public void CloseNotification()
+    {
+        gameObject.SetActive(false);
+    }
+}
