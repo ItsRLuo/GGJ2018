9444d28260e96cee511481b2e42f1fea6c00b414
richard <richard_r.l@hotmail.com>
2018-01-27 19:53:35 -0500

    moon Assets/_Game/Moons.meta

---
+++ Assets/pushButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTrigger(Collider other)
+    {
+        Debug.Log(other + " hit");
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z * -0.5f);
+    }
+}
