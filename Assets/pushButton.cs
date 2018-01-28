bdea6e3dee0c5616b2c9e55155a483e81928a505
richard <richard_r.l@hotmail.com>
2018-01-28 00:33:40 -0500

    fixed moon bug

--- Assets/pushButton.cs
+++ Assets/pushButton.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class pushButton : MonoBehaviour {
-    bool locked;
+    bool locked = false;
     // Use this for initialization
     void Start () {
-        Debug.Log("Asd");
-    }
-	
+     }
 	// Update is called once per frame
 	void Update () {
 	}
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("qqq");
-        this.gameObject.SetActive(false);
+        if (this.locked == false)
+        {
+            this.gameObject.SetActive(false);
+            this.locked = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        this.locked = false;
     }
 }
